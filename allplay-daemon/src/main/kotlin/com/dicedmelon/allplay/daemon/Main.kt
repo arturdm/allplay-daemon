2e1929b8eb69f3ea9199d4707b765280039c4b61
Artur Stepniewski <artur@dicedmelon.com>
2020-05-31 21:10:11 +0200

    Fix native libraries loading
    Works for both local and distribution runs

--- allplay-daemon/src/main/kotlin/com/dicedmelon/allplay/daemon/Main.kt
+++ allplay-daemon/src/main/kotlin/com/dicedmelon/allplay/daemon/Main.kt
@@ -2,7 +2,6 @@ package com.dicedmelon.allplay.daemon
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.File
 
 class Main {
     companion object {
@@ -11,60 +10,7 @@ class Main {
         @JvmStatic
         fun main(args: Array<String>) {
             logger.error(System.getProperty("java.library.path"))
-            tryLoadingNativeLibrary()
             App().main(args)
-        }
-
-        private fun tryLoadingNativeLibrary() {
-            try {
-                System.loadLibrary("alljoyn_java")
-            } catch (e: UnsatisfiedLinkError) {
-                loadLibrary()
-            }
-        }
-
-        private fun loadLibrary() {
-            val distDir = extractDistDirFromClasspath()
-            val libraryPath = createLibraryPath(distDir)
-            logger.info("Loading alljoyn_java from $libraryPath")
-            System.load(libraryPath)
-        }
-
-        private fun extractDistDirFromClasspath(): String {
-            val paths = System.getProperty("java.class.path").split(File.pathSeparator)
-            return paths.first { it.contains("tchaikovsky.jar") }
-                .substringBefore("/tchaikovsky.jar")
-        }
-
-        private fun createLibraryPath(distDir: String): String {
-            val separator = File.separator
-            val filename = createLibraryFilename()
-            return "${distDir}${separator}lib${separator}${osName()}${separator}${osArch()}${separator}${filename}"
-        }
-
-        private fun osName(): String {
-            return when (System.getProperty("os.name")) {
-                "Mac OS X", "darwin" -> "darwin"
-                "linux" -> "linux"
-                else -> throw RuntimeException("Couldn't extract supported os.name")
-            }
-        }
-
-        private fun osArch(): String {
-            return when (System.getProperty("os.arch")) {
-                "x86_64" -> "x86_64"
-                "i386", "x386" -> "x386"
-                else -> throw RuntimeException("Couldn't extract supported os.arch")
-            }
-        }
-
-        private fun createLibraryFilename(): String {
-            val libraryName = "alljoyn_java"
-            return when (System.getProperty("os.name")) {
-                "Mac OS X", "darwin" -> "lib${libraryName}.dylib"
-                "linux" -> "lib${libraryName}.so"
-                else -> throw RuntimeException("Couldn't create library filename with current os.name")
-            }
         }
     }
 }
